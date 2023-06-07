370022460791fdec554a6d9abee077afccfd026e
Julia Polugina <digggia@gmail.com>
2023-06-07 21:47:14 +0300

    Advanced select

--- SELECT.sql
+++ SELECT.sql
@@ -22,3 +22,75 @@ WHERE singer_name NOT LIKE '% %';
 SELECT track_name
 FROM Track
 WHERE track_name ILIKE '%мой%' OR track_name ILIKE '%my%';
+
+-- ========================
+
+SELECT g.genre_name, COUNT(DISTINCT gs.singer_id) AS singer_count
+FROM Genre g
+LEFT JOIN Genre_Singer gs ON g.genre_id = gs.genre_id
+GROUP BY g.genre_name;
+
+
+SELECT COUNT(*) AS track_count
+FROM Track t
+JOIN Album a ON t.album_id = a.album_id
+WHERE a.release_year BETWEEN '2019-01-01' AND '2020-12-31';
+
+
+SELECT a.album_name, AVG(t.duration) AS average_duration
+FROM Album a
+JOIN Track t ON a.album_id = t.album_id
+GROUP BY a.album_name;
+
+SELECT s.singer_name
+FROM Singer s
+LEFT JOIN Singer_Album sa ON s.singer_id = sa.singer_id
+LEFT JOIN Album a ON sa.album_id = a.album_id
+WHERE a.release_year <> '2020-01-01' OR a.release_year IS NULL;
+
+
+SELECT c.collection_name
+FROM Collection c
+JOIN Collection_Track ct ON c.collection_id = ct.collection_id
+JOIN Track t ON ct.track_id = t.track_id
+JOIN Album a ON t.album_id = a.album_id
+JOIN Singer_Album sa ON a.album_id = sa.album_id
+JOIN Singer s ON sa.singer_id = s.singer_id
+WHERE s.singer_name = 'Adele';
+
+-- ========================
+
+SELECT DISTINCT a.album_name
+FROM Album a
+JOIN Singer_Album sa ON a.album_id = sa.album_id
+JOIN Genre_Singer gs ON sa.singer_id = gs.singer_id
+GROUP BY a.album_name
+HAVING COUNT(DISTINCT gs.genre_id) > 1;
+
+SELECT t.track_name
+FROM Track t
+LEFT JOIN Collection_Track ct ON t.track_id = ct.track_id
+WHERE ct.track_id IS NULL;
+
+SELECT s.singer_name
+FROM Singer s
+JOIN Singer_Album sa ON s.singer_id = sa.singer_id
+JOIN Track t ON sa.album_id = t.album_id
+WHERE t.duration = (SELECT MIN(duration) FROM Track);
+
+SELECT a.album_name
+FROM Album a
+JOIN (
+    SELECT album_id, COUNT(*) AS track_count
+    FROM Track
+    GROUP BY album_id
+    HAVING COUNT(*) = (
+        SELECT MIN(track_count)
+        FROM (
+            SELECT COUNT(*) AS track_count
+            FROM Track
+            GROUP BY album_id
+        ) AS counts
+    )
+) AS min_tracks ON a.album_id = min_tracks.album_id;
+
